efae02c8f9d237a6d363b6a9251338254a41545b
thienphu <thienphu103@gmail.com>
2018-04-09 21:58:03 +0700

    login email google facebook

--- app/src/main/java/com/example/a/projectapptenno/SignUpActivity.java
+++ app/src/main/java/com/example/a/projectapptenno/SignUpActivity.java
@@ -1,16 +1,40 @@
 package com.example.a.projectapptenno;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
     ImageView img_back;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "SignUpActivity";
+    private ProgressDialog mProgressDialog;
+    private EditText edt_email;
+    private EditText edt_pass;
+    private EditText edt_repass;
+    private Button btn_register;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,19 +42,23 @@ public class SignUpActivity extends AppCompatActivity {
         initControl();
         initData();
         initEvent();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private void initControl() {
         img_back = (ImageView) findViewById(R.id.img_back_signup);
+        btn_register=(Button) findViewById(R.id.btn_signup);
+        edt_email = (EditText) findViewById(R.id.ext_email);
+        edt_pass = (EditText) findViewById(R.id.ext_nhapmatkhau);
+        edt_repass = (EditText) findViewById(R.id.ext_xacnhanmatkhau);
     }
 
 
     private void initData() {
-        img_back.setOnClickListener(new View.OnClickListener() {
+        btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent SignUp_Home = new Intent(SignUpActivity.this,LoginActivity.class);
-                startActivity(SignUp_Home);
+                createAccount();
             }
         });
     }
@@ -39,6 +67,141 @@ public class SignUpActivity extends AppCompatActivity {
         Window window = this.getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(this,R.color.my_statusbar_color));
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.my_statusbar_color));
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUISignUp(currentUser, null);
+    }
+
+    private void createAccount() {
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(edt_email.getText().toString(), edt_pass.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUISignUp(user, edt_email.getText().toString());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            showQuestionDialogError(task.getException().toString());
+                            updateUISignUp(null, null);
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = edt_email.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            edt_email.setError("Required.");
+            valid = false;
+        } else {
+            edt_email.setError(null);
+        }
+
+        String password = edt_pass.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            edt_pass.setError("Required.");
+            valid = false;
+        } else if (!(edt_pass.getText().toString().equals(edt_repass.getText().toString()))) {
+            edt_repass.setError("Password does not match the confirm password.");
+            valid = false;
+        } else {
+            edt_pass.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void updateUISignUp(FirebaseUser user, String email) {
+        hideProgressDialog();
+        if (user != null) {
+            showQuestionDialog(email);
+        } else {
+
+        }
+    }
+
+    public void showQuestionDialog(String NameUser) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("App");
+        builder.setMessage("Đăng kí thành công với email " + NameUser + " bạn có muốn login vào app không?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("Quay Lại Trang Login", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    public void showQuestionDialogError(String error) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("App");
+        builder.setMessage("" + error + "");
+        builder.setCancelable(false);
+
+        builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
 }
